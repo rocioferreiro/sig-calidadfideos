9b5ffacd8cce9a621e7b90d802b4e1794f08eb86
Rocio Ferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2022-09-09 14:05:19 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {get} from "./services/http";
+
+function App() {
+
+  const [successfulIntegration, setSuccesfulIntegration] = React.useState('');
+
+  React.useEffect(() => {
+    get('').then(res => {
+      setSuccesfulIntegration(res.info)
+    })
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          {successfulIntegration}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/services/http.jsx
@@ -0,0 +1,35 @@
+export const baseurl = process.env.NODE_ENV === 'production' ? "/api/" : 'http://localhost:3001/';
+
+const request = (url, method, body, config) => {
+
+    let headers = {
+        "Content-Type": "application/json", "Access-Control-Allow-Origin": "*"
+    };
+    const configuration = {
+        method: method, body: body ? JSON.stringify(body) : undefined, headers: headers,
+    };
+    return fetch(baseurl + url, configuration)
+
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+
+            // if an error occurs on the server return the errorMessage in case we intentionally threw that error, or a generic one in case an unexpected exception rises.
+            return response.json().then(error => {
+                if (error.hasOwnProperty('message')) {
+                    throw (error.message)
+                } else throw new Error("¡Error inesperado del servidor!")
+            })
+        })
+        // Catch connection errors and the error throw above.
+        .catch(error => {
+            throw(error)
+        })
+}
+
+export const get = (url, config = {}) => request(url, "GET", null, config);
+export const getWithBody = (url, body, config = {}) => request(url, "GET", body, config);
+export const post = (url, body, config = {}) => request(url, "POST", body, config);
+export const put = (url, body, config = {}) => request(url, "PUT", body, config);
+export const del = (url, config = {}) => request(url, "DELETE", null, config);
