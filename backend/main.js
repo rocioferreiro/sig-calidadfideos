db925c574471c9d9d7eebb042eeb7772e7dd5d3b
Rocio Ferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2022-09-22 13:12:58 -0300

    port

--- backend/main.js
+++ backend/main.js
@@ -3,7 +3,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./queries')
 const app = express()
-const port = 3001
 const cors = require('cors')
 
 app.use(cors())
