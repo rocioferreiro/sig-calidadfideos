52f09eba5baea0bdef646c30703a01a7bab39de1
Rocio Ferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2022-10-27 18:11:33 -0300

    demo changes

--- frontend/src/views/Tables/InProgressTable.tsx
+++ frontend/src/views/Tables/InProgressTable.tsx
@@ -86,20 +86,21 @@ export default function InProgressTable() {
   }
 
   function getSampleState(batch: { batch: Batch, changes: any[] }) {
+    if(batch.changes[0] === 0) return {visual: 'Falta agregar muestra', coccion: 'Falta control visual para desbloquear'}
     const state = batch.changes.filter(s => s.type === 'coccion').length > 0? 'coccion' : batch.changes.filter(s => s.type === 'visual').length > 0? 'visual': 'cargado';
     switch (state) {
       case 'cargado':
-        if(getDateXDaysAgo(2).getTime() > new Date(batch.batch.productionDate).getTime()){
+        if(getDateXDaysAgo(2).getTime() > new Date(batch.batch.samples[0].packingDate).getTime()){
           return {visual: 'Listo para control', coccion: 'Falta control visual para desbloquear'};
         } else {
-          const diff = getDifferenceBetweenDates(getDateXDaysAgo(2), new Date(batch.batch.productionDate))
+          const diff = getDifferenceBetweenDates(getDateXDaysAgo(2), new Date(batch.batch.samples[0].packingDate))
           return {visual: `${diff.amount} ${diff.unit} faltante/s`, coccion: 'Falta control visual para desbloquear'}
         }
       case 'visual':
         if(getDateXDaysAgo(7).getTime() > new Date(batch.batch.productionDate).getTime()){
           return {visual: `Listo, ${batch.batch.shatterLevel > 0? 'LEVE TRIZADO' : 'OK'}`, coccion: 'Listo para control'};
         } else {
-          const diff = getDifferenceBetweenDates(getDateXDaysAgo(7), new Date(batch.batch.productionDate))
+          const diff = getDifferenceBetweenDates(getDateXDaysAgo(7), new Date(batch.batch.samples[0].packingDate))
           return {visual: `Listo, ${batch.batch.shatterLevel > 0? 'LEVE TRIZADO' : 'OK'}`, coccion: `${diff.amount} ${diff.unit} faltante/s`}
         }
       case 'coccion':
